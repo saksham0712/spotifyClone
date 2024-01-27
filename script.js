0ac1b6c869fafa48eb8bdeeb0a6fb11333c408dd
Saksham Verma <110185108+saksham0712@users.noreply.github.com>
2024-01-27 14:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let currFolder;
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`./${folder}/`)
     let response = await a.text()
 
     let div = document.createElement('div')
@@ -58,7 +58,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums(){
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`./songs/`)
     let response = await a.text()
 
     let div = document.createElement('div')
